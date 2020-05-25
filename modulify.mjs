4e05d96bc092eae2775d91162b6879f3c602db45
Aditya Shankar <me@adityashankar.xyz>
2020-05-26 02:55:20 +0530

    added base code

--- modulify.mjs
+++ modulify.mjs
@@ -1,6 +1,12 @@
-export default async function mimport(path, baseurl){
-  if(!baseurl) baseurl = location.href
-  const url = new URL(path, baseurl)
+/**
+* import a js module as a normal module
+* basically this takes all new variables in the window object, out of the window object,
+* and returns them in an object
+*
+* @param {String} path - the url to import, cannot be a relative url
+*/
+export default async function mimport(path){
+  const url = new URL(path, location.href)
   const contentsResp = await fetch(url)
   const contents = await contentsResp.text()
   const beforeVars = new Set(Object.keys(window))
